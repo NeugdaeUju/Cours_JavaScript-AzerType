5952a923f1401251d360de95a25f97e84406123b
NeugdaeUju <leila.plltr@gmail.com>
2025-04-24 21:25:41 +0200

    Création des première conditions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello Word !")
+const listeMots = ["Cachalot" , "Pétunia" , "Serviette"]
+let score = 0
+
+let motUtilisateur = prompt("Ecrivez le mot : " + listeMots[0])
+
+if(motUtilisateur === listeMots[0]) {
+    score ++
+}
+
+console.log(motUtilisateur)
+console.log(score)
+
+
+
+motUtilisateur = prompt("Ecrivez le mot : " + listeMots[1])
+
+if(motUtilisateur === listeMots[1]) {
+    score ++
+}
+
+console.log(motUtilisateur)
+console.log(score)
+
+
+
+motUtilisateur = prompt("Ecrivez le mot : " + listeMots[2])
+
+if(motUtilisateur === listeMots[2]) {
+    score ++
+}
+
+console.log(motUtilisateur)
+console.log(score)
