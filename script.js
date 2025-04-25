1d47be9dbb9ed743efbb10fb9c4b306e5246fef7
Leïla Pelletier <leila.plltr@gmail.com>
2025-04-25 16:34:59 +0200

    Création des fonctions pour lancer le jeu

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
-const listeMots = ["Cachalot" , "Pétunia" , "Serviette"]
-const listePhrases = ["Pas de panique !" , "La vie, l'univers et le reste." , "Merci pour le poisson."]
-let score = 0
+// Mes variables
+    //Tableau contenant la liste des mots qui seront proposés au joueur
+    const listeMots = ["Cachalot" , "Pétunia" , "Serviette"]
 
-let choix = prompt("Veuillez choisir une liste : Mots ou Phrases .")
-while(choix !== "mots" && choix !== "phrases") {
-    choix = prompt("Veuillez choisir une liste : Mots ou Phrases .")
-}
+    //Tableau contenant la liste des phrases qui seront proposés au joueur
+    const listePhrases = ["Pas de panique !" , "La vie, l'univers et le reste." , "Merci pour le poisson."]
 
-if (choix === "mots") {
-    for(let i = 0 ; i < listeMots.length ; i++) {
-        let motUtilisateur = prompt("Ecrivez le mot : " + listeMots[i])
-        if(motUtilisateur == listeMots[i]) {
-            score++
+
+// Mes fonctions
+    //Fonction permettant d'afficher le résultat du jouer
+    function afficherResultat(score, nombreTotalMots){
+        console.log("Votre score est de " + score + " sur " + nombreTotalMots)
+    } 
+
+    //Fonction demandant au joueur de choisir la liste pour lancer le jeu
+    function choisirMotsOuPhrases() {
+        let choix = prompt("Veuillez choisir une liste : Mots ou Phrases .")
+        while(choix !== "mots" && choix !== "phrases") {
+            choix = prompt("Veuillez choisir une liste : Mots ou Phrases .")
         }
+        return choix
     }
-    console.log("Votre score est de " + score + " sur " + listeMots.length)
-} else {
-    for(let i = 0 ; i < listePhrases.length ; i++) {
-        let phraseUtilisateur = prompt("Ecrivez la phrase : " + listePhrases[i])
-        if(phraseUtilisateur == listePhrases[i]) {
-            score++
+
+    //Fonction contenant la boucle principale du jeu
+    function lancerBoucleDeJeu(listeProposition) {
+        let score = 0
+            for(let i = 0 ; i < listeProposition.length ; i++) {
+                motUtilisateur = prompt("Ecrivez le mot : " + listeProposition[i])
+                if(motUtilisateur === listeProposition[i]) {
+                    score++
+                }
+            }
+        return score
+    }
+
+    //Fonction permettant de lancer le jeu
+    function lancerJeu () {
+        let choix = choisirMotsOuPhrases()
+        let score = 0
+        let nombreTotalMots = 0
+
+        if (choix === "mots") {
+            score = lancerBoucleDeJeu(listeMots)
+            nombreTotalMots = listeMots.length
+        } else {
+            score = lancerBoucleDeJeu(listePhrases)
+            nombreTotalMots = listePhrases.length
         }
+
+       afficherResultat(score , nombreTotalMots)
+
     }
-    console.log("Votre score est de " + score + " sur " + listePhrases.length)
-}
+
+//Appelle de la fonction lancerJeu pour commencer à jouer
+lancerJeu()
