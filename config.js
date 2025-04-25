6c2a4913a9c04e06a76a2ad3140c98b7d6873965
Leïla Pelletier <leila.plltr@gmail.com>
2025-04-25 16:37:48 +0200

    Séparation du code JS : ajout d'un fichier config.js contant les tableaux de liste de jeux + ajout de la balise <script> dans le head du html.

---
+++ config.js
@@ -0,0 +1,6 @@
+// Mes variables
+    //Tableau contenant la liste des mots qui seront proposés au joueur
+    const listeMots = ["Cachalot" , "Pétunia" , "Serviette"]
+
+    //Tableau contenant la liste des phrases qui seront proposés au joueur
+    const listePhrases = ["Pas de panique !" , "La vie, l'univers et le reste." , "Merci pour le poisson."]
--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Mes variables
-    //Tableau contenant la liste des mots qui seront proposés au joueur
-    const listeMots = ["Cachalot" , "Pétunia" , "Serviette"]
-
-    //Tableau contenant la liste des phrases qui seront proposés au joueur
-    const listePhrases = ["Pas de panique !" , "La vie, l'univers et le reste." , "Merci pour le poisson."]
-
-
 // Mes fonctions
     //Fonction permettant d'afficher le résultat du jouer
     function afficherResultat(score, nombreTotalMots){
