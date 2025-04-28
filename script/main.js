0e424336aabda747405aaa6c47469413f89cd817
NeugdaeUju <leila.plltr@gmail.com>
2025-04-28 09:49:37 +0200

    Récupération des éléments HTML dans le fichier script

--- script/main.js
+++ script/main.js
@@ -1,2 +1,17 @@
 //Appelle de la fonction lancerJeu pour commencer à jouer
-lancerJeu()
+// lancerJeu()
+
+let motEcrit = document.getElementById("inputEcriture")
+console.log(motEcrit)
+
+let btnValidation = document.getElementById("btnValiderMot")
+console.log(btnValidation)
+
+let motPropose = document.querySelector(".zoneProposition")
+console.log(motPropose)
+
+let afficherScore = document.querySelector(".zoneScore span")
+console.log(afficherScore)
+
+let listeChoisis = document.querySelectorAll(".optionSource input")
+console.log(listeChoisis)
