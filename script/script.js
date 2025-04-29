900bd144d6eac8f1cbfe74d898d152a7b5bd2db4
NeugdaeUju <leila.plltr@gmail.com>
2025-04-29 10:58:38 +0200

    Ajout d'un EventListener sur les bouton radio pour changer la liste de jeu en foction du choix fait par le joueur

--- script/script.js
+++ script/script.js
@@ -17,28 +17,50 @@
     function lancerJeu () {
         let score = 0
         let i = 0
+        let listeProposition = listeMots
 
     let button = document.getElementById("btnValiderMot")
     let motUtilisateur = document.getElementById("inputEcriture")
-    afficherProposition(listeMots[i])
+    afficherProposition(listeProposition[i])
 
     button.addEventListener("click" , () => {
         console.log(motUtilisateur.value)
-        if (motUtilisateur.value === listeMots[i]) {
+        if (motUtilisateur.value === listeProposition[i]) {
             score++
         }
         i++
         afficherResultat(score , i)
 
         motUtilisateur.value = ``
-        if (listeMots[i] === undefined) {
+        if (listeProposition[i] === undefined) {
             afficherProposition("Le jeu est fini !")
             button.disabled = true
         } else {
-            afficherProposition(listeMots[i])
+            afficherProposition(listeProposition[i])
         }
 
     } )
 
+    let boutonChoix = document.querySelectorAll(".optionSource input")
+    for (let index = 0 ; index < boutonChoix.length ; index++) {
+        boutonChoix[index].addEventListener("change" , (event) => {
+            console.log(event.target.value)
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                listeProposition = listePhrases
+            }
+            afficherProposition(listeProposition[i])
+        })
+    }
+
     afficherResultat(score , i)
     }
+
+
+
+
+/*
+    if (boutonChoix[i].checked) {
+        choix = boutonChoix[i].value
+} */
